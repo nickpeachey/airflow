b18d0278bc5b68f28e31c0b1cb43952b4169d9cd
Nick Peachey <nick.peachey@gmail.com>
2025-05-27 11:32:37 +0100

    made changes

--- dags/spark_minio_saver-copy.py
+++ dags/spark_minio_saver-copy.py
@@ -4,7 +4,7 @@ from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKu
 from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
 
 with DAG(
-    dag_id="spark_minio_saver",
+    dag_id="spark_minio_saver-copy",
     start_date=datetime(2023, 1, 1),
     schedule_interval=None,
     catchup=False,
@@ -12,7 +12,7 @@ with DAG(
     submit_spark_job = SparkKubernetesOperator(
         # Do NOT set do_xcom_push=True
         do_xcom_push=False,  # Or remove the line entirely
-        task_id="submit_scala_job_minio",
+        task_id="submit_scala_job_minio-copy",
         namespace="default",
         application_file="spark-minio-copy.yaml",
         kubernetes_conn_id="kubernetes_default",
