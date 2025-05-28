913478e1f0da865f2b3e059055dd978c771eab81
Nick Peachey <nick.peachey@gmail.com>
2025-05-28 10:27:43 +0100

    uy

--- dags/spark_minio_saver.py
+++ dags/spark_minio_saver.py
@@ -30,7 +30,7 @@ with DAG(
 
     get_application_details_dag = PythonOperator(
         task_id='get_application_details',
-        python_callable=get_application_detaiggfls,
+        python_callable=get_application_details,
         provide_context=True,
     )
 
