3acffc0491085e7afc199ad98714dbdcddfb79c4
Nick Peachey <nick.peachey@gmail.com>
2025-05-28 10:06:03 +0100

    try this

--- dags/spark_minio_saver.py
+++ dags/spark_minio_saver.py
@@ -3,6 +3,14 @@ from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
 from airflow.hooks.base import BaseHook
+from airflow.operators.python import PythonOperator
+
+def get_application_details(**kwargs):
+    conn = BaseHook.get_connection('minio_conn')
+    print(f"Connection details: {conn.host}, {conn.login}, {conn.password}")
+        # This function can be used to retrieve details about the Spark application
+
+
 with DAG(
     dag_id="spark_minio_saver",
     start_date=datetime(2023, 1, 1),
@@ -10,6 +18,7 @@ with DAG(
     catchup=False,
 ) as dag:
     submit_spark_job = SparkKubernetesOperator(
+
         # Do NOT set do_xcom_push=True
         do_xcom_push=False,  # Or remove the line entirely
         task_id="submit_scala_job_minio",
@@ -17,6 +26,12 @@ with DAG(
         application_file="spark-minio.yaml",
         kubernetes_conn_id="kubernetes_default",
         in_cluster=True,
+    )
+
+    get_application_details_dag = PythonOperator(
+        task_id='get_application_details',
+        python_callable=get_application_details,
+        provide_context=True,
     )
 
     wait_for_spark_job = SparkKubernetesSensor(
@@ -29,14 +44,12 @@ with DAG(
         timeout=3600,
     )
 
-    def get_application_details(**kwargs):
-        conn = BaseHook.get_connection('minio_conn')
-        print(f"Connection details: {conn.host}, {conn.login}, {conn.password}")
-        # This function can be used to retrieve details about the Spark application
-        # if needed, but it is not required for the DAG to function.
-        pass
+
+
+
+
 
     # Example: A downstream task that depends on the Spark job's completion
     # downstream_task = ...
 
-    submit_spark_job >> wait_for_spark_job  # >> downstream_task
+    submit_spark_job >> wait_for_spark_job >> get_application_details_dag  # >> downstream_task
