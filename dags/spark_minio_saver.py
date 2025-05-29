cdee1ffa46826af1ef8106a2f4cab5b75b165be1
Nick Peachey <nick.peachey@gmail.com>
2025-05-29 07:52:19 +0100

    added sensor

--- dags/spark_minio_saver.py
+++ dags/spark_minio_saver.py
@@ -135,5 +135,16 @@ with DAG(
         # in the provided application_file dictionary.
     )
 
+      # Task to monitor the Spark job completion
+    monitor_spark_job = SparkKubernetesSensor(
+        task_id="monitor_scala_job_minio",
+        namespace="default",
+        application_name="{{ task_instance.xcom_pull(task_ids='generate_spark_minio_config_task', key='spark_app_name') }}",
+        kubernetes_conn_id="kubernetes_default",
+        in_cluster=True,
+        poke_interval=30,
+        timeout=600,  # 10 minutes timeout
+    )
+
     # Define the task dependencies
-    generate_spark_config_task >> submit_spark_job
+    generate_spark_config_task >> submit_spark_job >> monitor_spark_job
