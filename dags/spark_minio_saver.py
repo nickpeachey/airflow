ffa3f143cfdc0f7ea03379ab1e35e3db7bee73ca
Nick Peachey <nick.peachey@gmail.com>
2025-06-26 14:07:36 +0100

    fdsfds

--- dags/spark_minio_saver.py
+++ dags/spark_minio_saver.py
@@ -62,7 +62,7 @@ def generate_spark_minio_config(**kwargs):
             "spec": {
                 "type": "Scala",
                 "mode": "cluster",
-                "image": "nickpeachey/sparkminiosaver:4.0.2", # IMPORTANT: Replace with your actual Spark image (e.g., with Hadoop S3A support)
+                "image": "nickpeachey/sparkminiosaver:4.0.3", # IMPORTANT: Replace with your actual Spark image (e.g., with Hadoop S3A support)
                 "imagePullPolicy": "Always",
                 "mainClass": "com.cawooka.MainExecutor", # IMPORTANT: Replace with your Scala main class
                 "mainApplicationFile": "local:///opt/spark/jars/spark-debug-app.jar", # IMPORTANT: Path to your JAR inside the Spark image
