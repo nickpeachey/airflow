dae35885ecc76073564f215a249d376a62e162c3
Nick Peachey <nick.peachey@gmail.com>
2025-07-07 13:53:24 +0100

    fdfs

--- dags/another_spark_job.py
+++ dags/another_spark_job.py
@@ -67,7 +67,7 @@ def generate_spark_minio_config(**kwargs):
             "spec": {
                 "type": "Scala",
                 "mode": "cluster",
-                "image": "nickpeachey/another-spark-job", # IMPORTANT: Replace with your actual Spark image (e.g., with Hadoop S3A support)
+                "image": "nickpeachey/another-spark-job:0.0.1", # IMPORTANT: Replace with your actual Spark image (e.g., with Hadoop S3A support)
                 "imagePullPolicy": "Always",
                 "mainClass": "com.cawooka.MainExecutor", # IMPORTANT: Replace with your Scala main class
                 "mainApplicationFile": "local:///opt/spark/jars/another-spark-job.jar", # IMPORTANT: Path to your JAR inside the Spark image
