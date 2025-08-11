ffda152b252e779a2812a4c9a706b9e965012d4b
Nick Peachey <nick.peachey@gmail.com>
2025-08-11 15:23:36 +0100

    fdsfds

--- dags/spark_minio_saver_do_x_com.py
+++ dags/spark_minio_saver_do_x_com.py
@@ -73,3 +73,44 @@ def generate_spark_minio_config(**kwargs):
         json.dump(spark_application_config, f)
 
     ti = kwargs['ti']
+    ti.xcom_push(key='spark_app_file_xcom', value=file_path)
+    ti.xcom_push(key='spark_app_name_xcom', value=spark_app_name)
+
+
+with DAG(
+    dag_id="spark_minio_saver_do_x_com",
+    start_date=datetime(2023, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags=['spark', 'kubernetes', 'minio', 'connections'],
+) as dag:
+
+    start = EmptyOperator(task_id='start')
+
+    with TaskGroup("LocalStackJob", tooltip="Localstack Job") as localstack_job:
+
+        generate_spark_config_task = PythonOperator(
+            task_id='generate_spark_minio_config_task_spark_minio_saver_do_x_com',
+            python_callable=generate_spark_minio_config,
+        )
+
+        submit_spark_job = SparkKubernetesOperator(
+            task_id="submit_scala_job_minio_spark_minio_saver_do_x_com",
+            do_xcom_push=True,
+            namespace="default",
+            application_file="{{ ti.xcom_pull(task_ids='LocalStackJob.generate_spark_minio_config_task_spark_minio_saver_do_x_com', key='spark_app_file_xcom') }}",
+            kubernetes_conn_id="kubernetes_default",
+            in_cluster=True,
+        )
+
+        monitor_job = SparkKubernetesSensor(
+            task_id="monitor_spark_job_minio_spark_minio_saver_do_x_com",
+            application_name="{{ ti.xcom_pull(task_ids='LocalStackJob.generate_spark_minio_config_task_spark_minio_saver_do_x_com', key='spark_app_name_xcom') }}",
+            namespace="default",
+            kubernetes_conn_id="kubernetes_default",
+            do_xcom_push=True,
+        )
+
+        generate_spark_config_task >> submit_spark_job >> monitor_job
+
+    start >> localstack_job
